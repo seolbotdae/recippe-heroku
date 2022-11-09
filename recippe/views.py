5d1e6df8b3854bf7526be9280c7351cd6b1d5138
choeyoha <yoha6865@naver.com>
2022-11-09 13:13:47 +0900

    cors fix5

--- recippe/views.py
+++ recippe/views.py
@@ -28,14 +28,14 @@ from .mypage import *
 class LoginAPI(APIView):
     def post(self, request):
         print("type", type(request))
-        print(request.POST.get('uid'))
+        print(request.GET.get('uid'))
         #print(request.data['uid'])
         #upload
 
         #inputId = request.data['uid']
         #inputPw = request.data['password']
-        inputId = request.POST.get('uid')
-        inputPw = request.POST.get('password')
+        inputId = request.GET.get('uid')
+        inputPw = request.GET.get('password')
 
         controlLogin = ControlLogin_b()
         code, serializer = controlLogin.checkLogin(inputId, inputPw)
