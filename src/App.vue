a25ae90b676d4414475258b9d00e091969078622
nawhead <nawhead@yu.ac.kr>
2022-11-17 16:19:55 +0900

    1.2.2

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,7 @@ export default {
   created() {
     const UserInfo = JSON.parse(localStorage.getItem("UserInfo"));
     if(UserInfo.auto_login == false) {
+      console.log("오토 로그인 미선택 - 로그인 화면 이동");
       localStorage.removeItem("UserInfo");
       router.push({name: 'login'});
     }
