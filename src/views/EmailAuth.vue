b79ae8171e0e65e251d4ade7eebbec1ba302d3ec
nawhead <nawhead@yu.ac.kr>
2022-11-17 22:24:59 +0900

    1.3.0391

--- src/views/EmailAuth.vue
+++ src/views/EmailAuth.vue
@@ -91,14 +91,14 @@ export default {
         "email": this.info.email,
         "code": codenum
       });
+      const nextURL = this.next;
       console.log(checkInfo);
       herokuAPI.secondcheck(checkInfo)
         .then(function (response) {
           console.log("secondcheck", response);
-          console.log(this.next);
           if(response.status == 200) {
-            console.log("코드 일치", this.next);
-            router.push({name: this.next});
+            console.log("코드 일치", nextURL);
+            router.push({name: nextURL});
           }
         }) 
         .catch(function (e) {
