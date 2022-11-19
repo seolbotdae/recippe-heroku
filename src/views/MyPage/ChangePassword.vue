ec2ed9d926e5869ffd65fb8a3f0647ec2247cfdc
nawhead <nawhead@yu.ac.kr>
2022-11-19 23:11:21 +0900

    1.3.113

--- src/views/MyPage/ChangePassword.vue
+++ src/views/MyPage/ChangePassword.vue
@@ -3,18 +3,3 @@
     비밀번호 수정 화면
   </v-container>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      email: null,
-    }
-  },
-  mounted() {
-    this.email = localStorage.getItem("email");
-    console.log(this.email);
-    localStorage.removeItem("email");
-  }
-}
-</script>
