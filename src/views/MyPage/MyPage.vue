7a184817073448c1aa54ea717605f2c39190e898
whseol0203 <whseol0203@naver.com>
2022-11-20 00:25:50 +0900

    마이페이지 ui 추가

--- src/views/MyPage/MyPage.vue
+++ src/views/MyPage/MyPage.vue
@@ -1,9 +1,51 @@
 <template>
   <v-container>
-    마이페이지 기본 화면
-    <v-btn @click="changePW">비밀번호 변경</v-btn>
+    <v-layout>
+      <v-col offset="1" cols="10">
+        <v-card color="#f5efe6" class="py-2">
+          <v-card-title class="pl-10">나의 활동</v-card-title>
+          <v-card-text class="pl-15">내가 쓴 레시피</v-card-text>
+          <v-card-text class="pl-15">좋아요 누른 레시피</v-card-text>
+          <v-card-text class="pl-15">댓글 단 레시피</v-card-text>
+        </v-card>
+      </v-col>
+    </v-layout>
+
+    <v-layout>
+      <v-col offset="1" cols="5">
+        <v-card color="#f5efe6" class="py-2">
+          <v-card-title class="pl-10">냉장고</v-card-title>
+        </v-card>
+      </v-col>
+
+
+      <v-col cols="5">
+        <v-card color="#f5efe6" class="py-2">
+          <v-card-title class="pl-10">쪽지함</v-card-title>
+        </v-card>
+      </v-col>
+      
+    </v-layout>
+
+    <v-layout>
+      <v-col offset="1" cols="10" class="py-2">
+        <v-card color="#f5efe6" class="py-2">
+          <v-card-title class="ml-10">계정 관리</v-card-title>
+          <v-card-text class="ml-15">닉네임 변경</v-card-text>
+          <v-card-text class="ml-15">비밀번호 변경</v-card-text>
+          
+        </v-card>
+      </v-col>
+    </v-layout>
+
+
+    <!-- <v-btn @click="changePW">비밀번호 변경</v-btn> -->
   </v-container>
 </template>
+
+<style>
+
+</style>
 
 <script>
 import router from '@/router/index.js';
--- src/views/Recipe.vue
+++ src/views/Recipe.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <div>레시피 게시판</div>
-    <router-view />
-  </div>
+  <v-container>
+    <v-layout>
+      v
+    </v-layout>
+    
+    <div>
+      <div>레시피 게시판</div>
+      <router-view />
+    </div>
+  </v-container>
+  
 </template>
