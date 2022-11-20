849be4339f922da7f480b1f45639856565bff648
nawhead <nawhead@yu.ac.kr>
2022-11-20 23:20:14 +0900

    1.4.103

--- src/App.vue
+++ src/App.vue
@@ -15,8 +15,7 @@ export default {
   }),
 
   created() {
-    console.log("빌드를 하고 올리면 돌아야지?")
-    const UserInfo = JSON.parse(localStorage.getItem("UserInfo"));
+    const UserInfo = localStorage.getItem("UserInfo");
     console.log("App.vue created");
     if(UserInfo == null) {
       router.push({name: 'login'});
