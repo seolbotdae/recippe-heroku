a22ed44a97566e2e785c049608b6f8dd6b49ecaf
whseol0203 <whseol0203@naver.com>
2022-11-20 23:20:40 +0900

    비밀번호 변경 페이지 추가

--- src/views/MyPage/ChangePassword.vue
+++ src/views/MyPage/ChangePassword.vue
@@ -1,5 +1,51 @@
 <template>
   <v-container>
-    비밀번호 수정 화면
+    <v-layout>
+      <v-row justify="center">
+        <v-col cols="8">
+          <v-card color="#f5efe6" height="800">
+            <v-card color="#f5efe6" height="200" flat>
+              
+            </v-card>
+            <v-layout>
+              <v-row justify="center">
+                <v-col xl12 md10 cols="4">
+                  <v-card color="#fefefe" height="400" rounded="xl">
+                    <v-card-title id="title" class="pa-10 justify-center fontsize">비밀번호 변경</v-card-title>
+
+                    <v-col>
+                      <v-text-field filled label="새 비밀번호" placeholder="새로운 비밀번호 입력."></v-text-field>
+                      <v-text-field filled label="비밀번호 확인" placeholder="비밀번호 확인."></v-text-field>
+                    </v-col>
+
+                    
+                    <v-col>
+                      <v-row justify="center">
+                        <v-card-actions>
+                          <v-btn outlined width="100">취소</v-btn>
+                        </v-card-actions>
+                        <v-card-actions>
+                          <v-btn outlined width="100">변경</v-btn>
+                        </v-card-actions>
+                      </v-row>
+                      
+                    </v-col>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-layout>
+            
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-layout>
   </v-container>
 </template>
+
+<style>
+  #title{
+    font-weight: 700;
+    font-size: 2em;
+  }
+
+</style>
