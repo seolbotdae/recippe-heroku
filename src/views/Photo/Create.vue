572daf7f6ca5b19a676bb25fec80cf2fa195dc30
whseol0203 <whseol0203@naver.com>
2022-11-21 20:02:09 +0900

    pull 용

--- src/views/Photo/Create.vue
+++ src/views/Photo/Create.vue
@@ -47,9 +47,39 @@
                 </v-card>
               </v-col>
           </v-row>
+          <v-file-input
+            hide-input
+            truncate-length="15"
+          ></v-file-input>
           
+
+          <div id="app">
+            <input type="file" @change="onFileChange" />
+          
+            <div id="preview">
+              <img v-if="url" :src="url"/>
+            </div>
+          </div>
+
+
         </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
+
+<script>
+export default{
+  data(){
+    return{
+      url:null
+    }
+  },
+  methods: {
+    onFileChange(e) {
+      const file = e.target.files[0];
+      this.url = URL.createObjectURL(file);
+    }
+  }
+}
+</script>
