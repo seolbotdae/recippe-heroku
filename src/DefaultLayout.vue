809b5023a5f3a0f0f30bc6949f315d282da5d448
nawhead <nawhead@yu.ac.kr>
2022-11-23 18:35:30 +0900

    1.4.21

--- src/DefaultLayout.vue
+++ src/DefaultLayout.vue
@@ -13,7 +13,7 @@
       <v-spacer />
       <v-spacer />
 
-      <v-btn @click="logout">로그아웃</v-btn>
+      <v-btn @click="showDialog">로그아웃</v-btn>
 
       <v-spacer />
 
@@ -44,14 +44,55 @@
     >
       <div>푸터 위치입니다아</div>
     </v-footer>
+
+    <!-- 팝업창 형식 -->
+    <v-dialog
+      max-width="300"
+      v-model="popupDialog"
+    >
+      <popup-dialog
+        headerTitle = "로그아웃"
+        btnTitle="취소"
+        btn2Title="확인"
+        @hide="hideDialog"
+        @submit="checkDialog"
+      >
+        <template v-slot:body>
+          <!-- 내용이 들어가는 부분입니다아 -->
+          <div>로그아웃 하시겠습니까?</div>
+        </template>
+      </popup-dialog>
+    </v-dialog>
+    <!-- 팝업창 형식 -->
+
   </v-card>
 </template>
 
 <script>
 import router from '@/router/index.js';
+import PopupDialog from '@/components/popup.vue';
 
-export default {
+export default{
+  components: {
+    PopupDialog
+  },
+  data(){
+    return{
+      popupDialog: false,
+    }
+  },
   methods: {
+    showDialog(){ // 팝업창 보이기
+      this.popupDialog = true
+    },
+    hideDialog(){ // 팝업창 숨기기
+      this.popupDialog = false
+    },
+    checkDialog(){ // 팝업창 버튼 클릭시
+      // 확인 버튼 클릭시 동작 걸기
+      this.logout();
+      this.hideDialog();
+    },
     logout() {
       localStorage.removeItem("UserInfo");
       router.push({name: 'login'});
