50a4ad72687cf2e422beb6ff825dab24f8ba3bd9
nawhead <nawhead@yu.ac.kr>
2022-11-30 00:20:07 +0900

    temp

--- src/views/Recipe/Create.vue
+++ src/views/Recipe/Create.vue
@@ -7,7 +7,7 @@
           <!-- 뒤로 돌아가기 버튼 -->
           <v-btn text to="/recipe" class="ml-5 mt-5"> - 레시피 게시판</v-btn>
           <!-- 게시글 정보 입력란 -->
-          <v-form ref="form" lazy-validation class="mt-5">
+          <v-form ref="form" v-model="valid" lazy-validation class="mt-5">
             <div class="line mx-5"></div>
 
             <div class="px-10 d-flex align-center my-text">
@@ -214,6 +214,8 @@ export default{
   },
   data () {
     return {
+      valid: true,
+
       addIngredientDialog: false,
       popupDialog: false,
       categoryDialog: false,
@@ -321,7 +323,16 @@ export default{
 
     addRecipe() {
       const validate = this.$refs.form.validate();
-      if(!validate) return;
+      if(!validate) {
+        vm.snackbarContents = "모든 정보를 입력해주세요 (제목 또는 내용)"
+        vm.snackbar = true;
+        return;
+      }
+      if(this.ingredient.length == 0) {
+        vm.snackbarContents = "모든 정보를 입력해주세요 (식재료)"
+        vm.snackbar = true;
+        return;
+      }
       let vm = this;
       const UserInfo = JSON.parse(localStorage.getItem("UserInfo"));
       const recipe = JSON.stringify ({
